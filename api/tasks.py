883df3d0021f2cc76f9e5618eda433787f6cdb2f
mebr0 <aza17e@gmail.com>
2020-01-28 09:55:06 +0600

    Implement better search within single request

--- api/tasks.py
+++ api/tasks.py
@@ -1,38 +1,62 @@
 import requests
 
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
 from celery.task import periodic_task
 
 CITIES = ['ALA', 'MOW', 'TSE', 'LED', 'CIT']
-BASE_URI = 'https://api.skypicker.com/flights?fly_from={0}&fly_to={1}&date_from={2}&date_to={2}&partner=mebr0&curr=KZT&asc=1'
+BASE_URI = 'https://api.skypicker.com/flights?fly_from={0}&fly_to={1}&date_from={2}&date_to={3}&partner=mebr0&curr=KZT&asc=1'
+D = {}
 
 
 @periodic_task(run_every=(timedelta(minutes=1)), name='taskA', ignore_result=True)
 def do_task():
 
-    for i in range(len(CITIES)):
-        for j in range(i+1, len(CITIES)):
-            print('Doing from ' + CITIES[i] + ' to ' + CITIES[j])
-            do_single_direction(CITIES[i], CITIES[j])
+    start = datetime.now().replace(microsecond=0)
+
+    # for i in range(len(CITIES)):
+    #     for j in range(i+1, len(CITIES)):
+    #         print('Doing from ' + CITIES[i] + ' to ' + CITIES[j])
+    #         do_single_direction(CITIES[i], CITIES[j])
+
+    do_single_direction(CITIES[0], CITIES[1])
+
+
+    for key in D:
+        if D.get(key):
+            print(key + ' --- ' + str(D.get(key).get('price')))
+        else:
+            print(key + ' --- ' + '0')
+
+    end = datetime.now().replace(microsecond=0)
+
+    print(end - start)
 
 
 def do_single_direction(fly_from, fly_to):
     today = date.today()
+    end = today + timedelta(days=30)
 
-    # date_string = today.strftime('%d/%m/%Y')
-    #
-    # do_one_day(fly_from, fly_to, date_string)
+    date_from = today.strftime('%d/%m/%Y')
+    date_to = end.strftime('%d/%m/%Y')
 
-    for i in range(30):
-        date_string = today.strftime('%d/%m/%Y')
-
-        do_one_day(fly_from, fly_to, date_string)
+    for i in range(31):
+        D[str(today.strftime('%d/%m/%Y'))] = None
 
         today += timedelta(days=1)
 
+    do_one_day(fly_from, fly_to, date_from, date_to)
 
-def do_one_day(fly_from, fly_to, date_string):
-    response = requests.get(BASE_URI.format(fly_from, fly_to, date_string))
+
+    # for i in range(30):
+    #     date_string = today.strftime('%d/%m/%Y')
+    #
+    #     do_one_day(fly_from, fly_to, date_string)
+    #
+    #     today += timedelta(days=1)
+
+
+def do_one_day(fly_from, fly_to, date_from, date_to):
+    response = requests.get(BASE_URI.format(fly_from, fly_to, date_from, date_to))
 
     if response.status_code != 200:
         print('Not success code! ' + str(response.status_code))
@@ -44,8 +68,18 @@ def do_one_day(fly_from, fly_to, date_string):
         print('Data is empty!')
         return
 
-    ticket = response.json().get('data')[0]
+    tickets = response.json().get('data')
 
-    minimum = {'price': ticket.get('price'), 'booking_token': ticket.get('booking_token')}
+    for ticket in tickets:
+        current_date = datetime.utcfromtimestamp(ticket.get('dTimeUTC')).strftime('%d/%m/%Y')
 
-    print(date_string + ' --- ' + str(minimum.get('price')))
+        if D[str(current_date)] == None:
+            D[str(current_date)] = {'price': ticket.get('price'), 'booking_token': ticket.get('booking_token')}
+
+    # ticket = response.json().get('data')[0]
+
+    # print('date is ' + datetime.utcfromtimestamp(ticket.get('dTimeUTC')).strftime('%d/%m/%Y'))
+
+    # minimum = {'price': ticket.get('price'), 'booking_token': ticket.get('booking_token')}
+
+    # print(date_string + ' --- ' + str(minimum.get('price')))
