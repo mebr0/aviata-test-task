021049813a34830ac9f77433506de662c6a0fbe4
mebr0 <aza17e@gmail.com>
2020-04-01 16:49:45 +0600

    Rework whole code by reviwe, add README and test task docx

--- api/tasks.py
+++ api/tasks.py
@@ -1,112 +1,83 @@
-import requests
-
 from datetime import timedelta, date, datetime
+
+import requests
 from celery.task import periodic_task
-from djcelery.backends.cache import cache
+from django.core.cache import cache
 
-CITIES_1 = ['ALA', 'TSE', 'ALA', 'MOW', 'ALA', 'CIT', 'TSE', 'MOW', 'TSE', 'LED']
-CITIES_2 = ['TSE', 'ALA', 'MOW', 'ALA', 'CIT', 'ALA', 'MOW', 'TSE', 'LED', 'TSE']
-FORMAT = '%d/%m/%Y'
-LIMIT = 5
+CITIES_1 = ['ALA', 'ALA', 'ALA', 'TSE', 'TSE']  # All pairs of cities
+CITIES_2 = ['TSE', 'MOW', 'CIT', 'MOW', 'LED']
+FORMAT = '%d/%m/%Y'                             # Date format
+LIMIT = 5                                       # Requests limit
 
-BASE_URI_FLIGHTS = 'https://api.skypicker.com/flights?fly_from={0}&fly_to={1}&date_from={2}&date_to={3}&' \
-                   'partner=mebr0&curr=KZT&asc=1'
-BASE_URI_CHECK = 'https://booking-api.skypicker.com/api/v0.1/check_flights?v=2&booking_token={0}&partner=mebr0&' \
-                 'bnum=3&pnum=2&affily=picky_market&currency=KZT'
+FLIGHTS_URI = 'https://api.skypicker.com/flights'
+CHECK_URI = 'https://booking-api.skypicker.com/api/v0.1/check_flights'
 
 
-@periodic_task(run_every=(timedelta(minutes=9)), name='search_tickets')
+@periodic_task(run_every=(timedelta(days=1)), name='search_tickets')
 def search_tickets():
-    cache.clear()
 
-    start = datetime.now().replace(microsecond=0)
-
-    for i in range(len(CITIES_1)):
-        print('Doing from ' + CITIES_1[i] + ' to ' + CITIES_2[i])
-        search_single_direction(CITIES_1[i], CITIES_2[i])
-
-    # for key in cache.keys('*'):
-    #     print(key + ' --- ' + str(cache.get(key)))
-
-    end = datetime.now().replace(microsecond=0)
-
-    print(str(end - start))
-
-
-def search_single_direction(fly_from, fly_to):
     today = date.today()
     date_from = today.strftime(FORMAT)
 
     end = today + timedelta(days=30)
     date_to = end.strftime(FORMAT)
 
-    search_single_direction_dates(fly_from, fly_to, date_from, date_to)
+    for first, second in zip(CITIES_1, CITIES_2):
+        search_single_direction(first, second, date_from, date_to)
+        search_single_direction(second, first, date_from, date_to)
 
 
-def search_single_direction_dates(fly_from, fly_to, date_from, date_to):
-    response = requests.get(BASE_URI_FLIGHTS.format(fly_from, fly_to, date_from, date_to))
+def search_single_direction(fly_from, fly_to, date_from, date_to):
+    params = {'fly_from': fly_from, 'fly_to': fly_to, 'date_from': date_from, 'date_to': date_to, 'partner': 'mebr0',
+              'curr': 'KZT', 'asc': 1}
+    response = requests.get(FLIGHTS_URI, params=params, timeout=120)
 
     if response.status_code != 200:
-        print('Not success code! ' + str(response.status_code))
-        return
-
-    data = response.json().get('data')
-
-    if len(data) == 0:
-        print('Data is empty!')
         return
 
     tickets = response.json().get('data')
 
+    if len(tickets) == 0:
+        return
+
+    data = {}
+
     for ticket in tickets:
         current_date = datetime.utcfromtimestamp(ticket.get('dTimeUTC')).strftime(FORMAT)
 
-        if cache.get(str(current_date)) is None:
+        key = f'{current_date}_{fly_from}_{fly_to}'
+
+        if data.get(key) is None:
 
             if check_ticket(ticket.get('booking_token')) != 0:
                 break
 
-            their_tickets = [{'from': fly_from, "to": fly_to, 'price': ticket.get('price'),
-                              'booking_token': ticket.get('booking_token')}]
-            cache.set(str(current_date), their_tickets)
+            data[key] = {'price': ticket.get('price'), 'booking_token': ticket.get('booking_token')}
 
         else:
-            their_tickets = cache.get(str(current_date))
+            their_ticket = data.get(key)
 
-            found = False
+            if their_ticket.get('price') > ticket.get('price'):
 
-            for their_ticket in their_tickets:
-                if their_ticket.get('from') == fly_from and their_ticket.get('to') == fly_to:
+                if check_ticket(ticket.get('booking_token')) != 0:
+                    break
 
-                    found = True
+                data[key] = {'price': ticket.get('price'), 'booking_token': ticket.get('booking_token')}
 
-                    if their_ticket.get('price') > ticket.get('price'):
-
-                        if check_ticket(ticket.get('booking_token')) != 0:
-                            break
-
-                        their_tickets.remove(their_ticket)
-
-                        their_tickets.append({'from': fly_from, "to": fly_to, 'price': ticket.get('price'),
-                                              'booking_token': ticket.get('booking_token')})
-
-                        break
-
-            if not found and check_ticket(ticket.get('booking_token')) == 0:
-                their_tickets.append({'from': fly_from, "to": fly_to, 'price': ticket.get('price'),
-                                      'booking_token': ticket.get('booking_token')})
-
-            cache.set(str(current_date), their_tickets)
+    cache.set('tickets', data, 86400)
 
 
 def check_ticket(booking_token):
     response = None
     checked = False
 
+    params = {'booking_token': booking_token, 'v': 2, 'partner': 'mebr0', 'bnum': 3, 'pnum': 2,
+              'affily': 'picky_market', 'currency': 'KZT'}
+
     i = 0
 
     while not checked:
-        response = requests.get(BASE_URI_CHECK.format(booking_token))
+        response = requests.get(CHECK_URI, params=params, timeout=15)
         checked = response.json().get('flights_checked')
 
         i += 1
@@ -114,17 +85,13 @@ def check_ticket(booking_token):
         if i >= LIMIT:
             return 1
 
-    data = response.json()
-
-    print("Checking! " + booking_token[:10])
-
-    invalid = data.get('flights_invalid')
-    price_change = data.get('price_change')
+    invalid = response.json().get('flights_invalid')
+    price_change = response.json().get('price_change')
 
     if invalid:
         return 1
-    # Not implemented :)
-    elif price_change:
+
+    elif price_change:  # Not implemented :)
         return 2
 
     return 0
